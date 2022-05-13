bf5476474e89e00ef042b06b7db6aee1c901de08
Jyotirmoy <acharjee4381@gmail.com>
2022-05-13 21:00:47 +0600

    commited

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port = process.env.PORT || 5000
-// const port = process.env.PORT
+// const port = process.env.PORT || 5000
+const port = process.env.PORT
 require('dotenv').config()
 
 
